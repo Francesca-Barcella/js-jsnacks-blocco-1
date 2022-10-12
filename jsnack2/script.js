80ace2639e1369c9f5779d2c72c849d8923db978
Francesca-Barcella <pollon.fra@gmail>
2022-10-12 12:09:32 +0200

    snack 02 - scritto e stampato in console

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,1 +1,29 @@
 //alert ('ciao, funziono!')
+
+
+/******************************************
+TRACCIA 2
+L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga. 
+*****************************************/
+
+// prompt parola 1
+// prompt parola 2
+// .lenght per le lunghezze
+// if else
+// stamp in console
+
+const firstWord = prompt('Inserisci una parola');
+const secondWord = prompt('Inserisci una seconda parola');
+console.log(firstWord, secondWord);
+
+
+if (firstWord.length < secondWord.length) {
+    console.log(firstWord);
+    console.log(secondWord);
+} else {
+    console.log(secondWord);
+    console.log(firstWord);
+}
+
+
