420645db270ca9da7b50af6594c5bdcb92af343f
Francesca-Barcella <pollon.fra@gmail>
2022-10-12 11:53:02 +0200

    jsnack - 1 stampa in dom dei due numeri scelti

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -10,10 +10,16 @@
 // verificare il numero più alto - if / else
 // stampare in console il numero più alto
 
+let numberPlayer1 = document.getElementById('first_number');
+let numberPlayer2 = document.getElementById('second_number');
+
+
 // chiedere 1° numero all'utente - prompt con const
 const firstNumber = Number(prompt('Scegli un numero'));
+numberPlayer1.innerHTML=('il primo numero è ', firstNumber);
 // chiedere 2° numero all'utente - prompt con const
 const secondNumber = Number(prompt('Scegli un numero'));
+numberPlayer2.innerHTML=('il primo numero è ', secondNumber);
 console.log(firstNumber, secondNumber);
 
 // creare variabile per individuare il numero più alto
