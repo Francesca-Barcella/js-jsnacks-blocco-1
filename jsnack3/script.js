1f374af6b2906a1e5c7bb45fc2f1c93c3e6562f8
Francesca-Barcella <pollon.fra@gmail>
2022-10-13 12:05:08 +0200

    SNACK 03 - initial commit

---
+++ jsnack3/script.js
@@ -0,0 +1,23 @@
+//alert ('ciao, funziono!')
+
+
+/******************************************
+TRACCIA 3.b
+
+Snack 3 blocco 1 con While:
+Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+
+*****************************************/
+alert('ciao')
+
+let i = 0;
+
+while ( i < 10) {
+    const userNumber = prompt('inserisci un numero');
+    console.log(userNumber);
+    
+}
+
+
+
